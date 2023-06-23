ecff0b2726d66407acb619e7224812eee06d9a6b
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-06-23 14:11:18 +0300

    initial: init picture_resizer project

--- api/apps.py
+++ api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ResizerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "resizer"
+    name = "api"
--- picture_resizer/settings.py
+++ picture_resizer/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "restframework",
-    "resizer.apps.ResizerConfig",
+    "api.apps.ResizerConfig",
 ]
 
 MIDDLEWARE = [

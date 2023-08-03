6b9afccaf050effabb0b39c1252c912369a2c464
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-08-03 14:17:37 +0300

    feature: resize_image call moved to signal pre_save_picture, file name generation moved to property method generate_file_name

--- picture/models.py
+++ picture/models.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from io import BytesIO
 from hashlib import md5
 from django.db import models
 from PIL import Image as PilImage
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
 class Picture(models.Model):
@@ -18,25 +21,28 @@ class Picture(models.Model):
     width = models.PositiveIntegerField()
     height = models.PositiveIntegerField(null=True, blank=True)
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        self.resize_image()
+    def resize_image(self, file_name):
+        image = PilImage.open(self.file)
+        if self.height is None:
+            self.height = int(image.height / image.width * self.width)
+        resized_image = image.resize((self.width, self.height))
+        output = BytesIO()
+        resized_image.save(output, format=image.format, quality=100)
+        output.seek(0)
+        self.resized_image = InMemoryUploadedFile(
+            output, 'ImageField',
+            file_name,
+            'image/jpeg', output.getbuffer().nbytes, None
+        )
 
-    def resize_image(self):
-        if not self.resized_image:
-            image = PilImage.open(self.file)
-            if self.height is None:
-                self.height = int(image.height / image.width * self.width)
-            resized_image = image.resize((self.width, self.height))
-            output = BytesIO()
-            resized_image.save(output, format=image.format, quality=100)
-            output.seek(0)
-            original_file_name = self.file.name.split(".")[0]
-            file_name = (f'{md5(original_file_name.encode()).hexdigest()}_'
-                         f'{self.width}x{self.height}.{image.format}')
-            self.resized_image = InMemoryUploadedFile(
-                output, 'ImageField',
-                file_name,
-                'image/jpeg', output.getbuffer().nbytes, None
-            )
-            super().save()
+    @property
+    def generate_file_name(self):
+        file_path = Path(self.file.name)
+        return (f'{md5(file_path.name.encode()).hexdigest()}_'
+                f'{self.width}x{self.height}{file_path.suffix}')
+
+
+@receiver(pre_save, sender=Picture)
+def pre_save_picture(sender, instance, **kwargs):
+    if not instance.resized_image:
+        instance.resize_image(instance.generate_file_name)
